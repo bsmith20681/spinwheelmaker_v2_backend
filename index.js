6cd2dc28f3be82075baa47e39ccf18a9fc59e41b
Ben Smith <bsmith20681@gmail.com>
2023-03-25 15:20:45 -0400

    updated cors

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(cors());
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", `${process.env.CLIENT_URL}`);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Credentials", "true");
 
