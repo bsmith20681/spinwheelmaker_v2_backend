80563618a751681a1c3e35b65cdc8b26d884acb4
Ben Smith <bsmith20681@gmail.com>
2023-03-25 21:17:31 -0400

    added trust proxy

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ app.use(cookieParser());
 
 app.use(cors());
 
+app.set("trust proxy", 1);
+
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", `${process.env.CLIENT_URL}`);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
